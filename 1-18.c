a8f0a0624e2f9d4d2c6fad32c12b0d86c828e0aa
Austin Keil <austin.c.keil@gmail.com>
2018-12-13 11:51:20 -0800

    added 1-18.c

---
+++ 1-18.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int GetStrippedLine(char line[], int lim); 
+
+int main()
+{
+  int len;         
+  char line[MAXLINE];   
+
+  while ((len = GetStrippedLine(line, MAXLINE)) > 0) {
+    printf("%s", line);
+  }
+  return 0; 
+}
+
+// returns 0 if blank line;
+int GetStrippedLine(char s[], int lim)
+{
+  int c, i;
+  
+  for(i=0; i < lim-1 && (c=getchar()) != EOF && c!='\n'; ++i)
+    s[i] = c; 
+  s[i] = '\n';
+  while(i > 0 && s[i] == '\n' || s[i] == '\t' || s[i] == ' '){
+    --i;
+  }
+  if (i == 0) {
+    s[i++] = '\0';
+  } else {
+    s[++i] = '\n';
+    s[++i] = '\0';
+  }
+  return i;
+}
